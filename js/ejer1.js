220041901197998e5058cb91296080900493a738
mauroarms <mauro.armas14@gmail.com>
2023-10-19 12:54:36 -0300

    Se realizo el ejercicio 1 correctamente

---
+++ js/ejer1.js
@@ -0,0 +1,7 @@
+let numero1 = 10;
+let numero2 = 8;
+
+console.log("La suma de numero1 y numero 2 es", numero1+numero2);
+console.log("La resta de numero1 y numero 2 es", numero1-numero2);
+console.log("La multiplicación de numero1 y numero 2 es", numero1*numero2);
+console.log("La división de numero1 y numero 2 es", numero1/numero2);
