fb6d3353a2539a98369a7313c24da434fa5313bb
mauroarms <mauro.armas14@gmail.com>
2023-10-19 13:25:21 -0300

    Se realizo el ejercicio 5 Correctamente

---
+++ js/ejer5.js
@@ -0,0 +1,8 @@
+/*5.- Escribe un programa de tres líneas que pida un número, pida otro número y escriba el resultado de sumar estos dos números.
+Nota: Tener en cuenta la siguiente función: parseInt
+https://developer.mozilla.org/es/docs/Web/JavaScript/Referencia/Objetos_globales/parseInt
+*/
+
+let num1 = parseInt(prompt("Ingrese el primer numero a sumar:"))
+let num2 = parseInt(prompt("Ingrese el segundo numero a sumar:"))
+console.log("El resultado de sumar "+num1+" y "+num2+" es igual a "+(num1+num2))
